5a0b1df462e2908a7991781c72e73e80bf9c31df
eleyna <eleyna@gmail.com>
2013-06-27 11:00:51 -0400

    can view favorites and add a favorite

--- main.rb
+++ main.rb
@@ -4,5 +4,19 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
-get '/' do
+get '/favorites' do
+  @favorite_movie_list = File.new('movies.csv', 'r')
+  erb :favorites
+end
+
+get '/new_movie' do
+  erb :new_movie
+end
+
+post '/new_movie' do
+  @new_movie = params[:new_movie]
+  favorite_movie_list = File.new('movies.csv', 'a+')
+  favorite_movie_list.puts @new_movie
+  favorite_movie_list.close
+  redirect to('/favorites')
 end
